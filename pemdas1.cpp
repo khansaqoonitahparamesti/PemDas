88765282f85ccbccfe68cdb849c14d6657e0dc12
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2025-03-04 19:43:05 +0700

    pertemuan1

---
+++ pemdas1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ using namespace std;
+
+ float Luas, Panjang, Lebar;
+
+ void InputData()
+ {
+    cout << "Masukan panjangnya : ";
+    cin >> Panjang;
+    cout << "Masukin lebarnya : ";
+    cin >> Lebar;
+ }
+ float HitungLuas()
+ {
+    return Panjang * Lebar;
+ }
+ void TampilData()
+ {
+    cout << "Luas Persegi Panjang : " << HitungLuas();
+ }
+
+ int main()
+ {
+    InputData();
+    TampilData();
+ }
